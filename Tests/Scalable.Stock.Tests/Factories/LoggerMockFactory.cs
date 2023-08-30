27df1454fbc9c382a088c822259da9b74af4d367
Dani Bautista <Danielbahe@gmail.com>
2023-08-30 08:48:50 +0200

    change moq for NSubstitute

--- Tests/Scalable.Stock.Tests/Factories/LoggerMockFactory.cs
+++ Tests/Scalable.Stock.Tests/Factories/LoggerMockFactory.cs
@@ -1,25 +1,13 @@
-﻿using Moq;
-using Serilog;
+﻿using Serilog;
+using NSubstitute;
 
 namespace Scalable.Stock.Tests.Factories
 {
     internal class LoggerMockFactory
     {
-        public static IMock<ILogger> GetLoggerMock()
+        public static ILogger GetLoggerMock()
         {
-            var logger = new Mock<ILogger>();
-            logger.Setup(l => l.Information(It.IsAny<string>()));
-            logger.Setup(l => l.Information(It.IsAny<string>(), It.IsAny<object>()));
-            logger.Setup(l => l.Debug(It.IsAny<string>()));
-            logger.Setup(l => l.Debug(It.IsAny<string>(), It.IsAny<object>()));
-            logger.Setup(l => l.Warning(It.IsAny<string>()));
-            logger.Setup(l => l.Warning(It.IsAny<string>(), It.IsAny<object>()));
-            logger.Setup(l => l.Error(It.IsAny<string>()));
-            logger.Setup(l => l.Error(It.IsAny<string>(), It.IsAny<object>()));
-            logger.Setup(l => l.Verbose(It.IsAny<string>()));
-            logger.Setup(l => l.Verbose(It.IsAny<string>(), It.IsAny<object>()));
-
-            return logger;
+            return Substitute.For<ILogger>();
         }
     }
 }
